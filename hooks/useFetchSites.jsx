90ccdde2abb396c140bb043e15e41d90d20945a2
Alber0087 <albertorg0087@gmail.com>
2023-07-15 13:46:21 +0100

    New api key

--- hooks/useFetchSites.jsx
+++ hooks/useFetchSites.jsx
@@ -1,5 +1,6 @@
 /*       'X-RapidAPI-Key': 'ac9f308512mshe38117e6bda849ep178b23jsncab96a46572c', */
 /*       'X-RapidAPI-Key': '3aec6ecd4bmshe1eb0818fc5277cp17b368jsndf982a585105'  */
+/*       'X-RapidAPI-Key': '74fddfd9cfmsheba8007d8f1d840p19f49bjsn91a9898a1080'  */
 
 import { QueryClient, useQuery } from '@tanstack/react-query'
 import axios from 'axios'
@@ -22,7 +23,7 @@ export async function fetchSites(type, sw, ne) {
         },
         headers: {
           'X-RapidAPI-Key':
-            '74fddfd9cfmsheba8007d8f1d840p19f49bjsn91a9898a1080',
+            '4ade9ea85bmsh11eec07308a68efp1c6f18jsna5b41a20be08',
           'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
         },
       }
